21876cda85928123870edf3428b27ca49c58abd2
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-02-11 20:46:06 +0000

    update main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Website :   https://engineerdanny.me
 Date    :   February, 2022
 """
 
+import sys
 import requests
 from bs4 import BeautifulSoup
 from colored import fg, bg, attr
@@ -24,107 +25,123 @@ headers = {
     'Connection': 'keep-alive',
 }
 
-print('%s%s Hello, Welcome to APK Downloader 🙌 !!! %s' %
-      (fg('white'), bg('green'), attr('reset')))
-# take the package_id from the user
-package_id = input(
-    "Enter the packageId of the android app\nShould something like com.example.app\n")
 
-# verify g_play_url with packageId string
-g_play_res = requests.get(g_play_url + package_id,
-                          headers=headers, allow_redirects=True)
-if(g_play_res.status_code != 200):
+def show_internet_error():
+    print(fg('red') + '\n[!] ERROR: ' + attr('reset') +
+          'Check your internet connection and try again.\n')
+    exit()
+
+
+def show_arg_error():
+    print(fg('red') + '\n[!] ERROR: ' + attr('reset') +
+          'Invalid Format\nShould be of the format `python main.py {{PACKAGE_ID}}`')
+    exit()
+
+
+def show_invalid_id_err():
     print('%s%s PackageId is invalid %s' %
           (fg('white'), bg('red'), attr('reset')))
     exit()
 
-response = requests.get(search_url + package_id,
-                        headers=headers, allow_redirects=True)
-statusCode = response.status_code
-content = response.content
 
-# check the statuscode and verify if the packageId is valid
-if statusCode == 200:
-    print("PackageId is valid")
-else:
-    print("PackageId is invalid")
+def main():
+    # Get the argument from the command line
+    if len(sys.argv) != 2:
+        show_arg_error()
+
+    print('%s%s Hello, Welcome to APK Downloader 🙌 !!! %s' %
+          (fg('white'), bg('green'), attr('reset')))
+
+    # take the package_id from the user
+    package_id = sys.argv[1]
+
+    # verify g_play_url with packageId string
+    g_play_res = requests.get(g_play_url + package_id,
+                              headers=headers, allow_redirects=True)
+    if(g_play_res.status_code != 200):
+        show_invalid_id_err()
+
+    # search the web page using the package id
+    search_res = requests.get(search_url + package_id,
+                              headers=headers, allow_redirects=True)
+
+    # check the statuscode and verify it
+    if search_res.status_code != 200:
+        show_internet_error()
+
+    # there will be a list of apps that show on the website
+    soup = BeautifulSoup(search_res.content, 'html.parser')
+
+    # # find the first class with the class name "search-dl"
+    # children = soup.findChildren('a', class_='col col-6 list')
+
+    # # get href of the first child
+    # first_child = children[0]
+    # app_href = first_child.get('href')
+
+    # # get the name of the child
+    # app_name = first_child.find('strong').text
+
+    # # hyphenate and format the app name into lowercase
+    # app_name_formatted = app_name.replace(" ", "-").lower()
+    # app_url = version_url + app_href
+
+    # # get string after the last '/'
+    # app_id = app_href.rsplit('/', 1)[1]
+
+    # app_response = requests.get(version_url + app_id,
+    #                             headers=headers, allow_redirects=True)
+
+    # if app_response.status_code != 200:
+    #     show_internet_error()
+
+    tbody_children = soup.findAll('a')
+
+    sub_dl_links = []
+
+    # tbody_children
+    # loop through the children and get the href
+    for item in tbody_children:
+        # get href of the child
+        link = item.get('href')
+        # if link contains "downwload"
+        if link.find("/download/") != -1:
+            # append the link to the list
+            sub_dl_links.append(base_url+link)
+
+    # establish a connection to the first link
+    sub_dl_res = requests.get(sub_dl_links[0],
+                              headers=headers, allow_redirects=True)
+    if sub_dl_res.status_code != 200:
+        show_internet_error()
+
+    # locate the script, get the contents
+    script_text = BeautifulSoup(
+        sub_dl_res.content, 'html.parser').findAll("script")
+
+    # find the last but one script tag
+    last_script = script_text[-2].contents[0]
+
+    # query the token from the script
+    token = re.findall("token','([^\']+)", last_script)[0]
+
+    # make a request to the download link
+    dl_res = requests.post(dl_url, data={'token': token}, headers=headers)
+    if dl_res.status_code != 200:
+        show_internet_error()
+    dl_res_json = dl_res.json()
+    # get the download_link from the json response
+    download_link = dl_res_json['download_link']
+
+    # download the apk
+    print("Downloading APK")
+    exit()
+    r = requests.get(download_link, allow_redirects=True)
+    with open(app_name_formatted+'.apk', 'wb') as f:
+        f.write(r.content)
+    print("APK Downloaded")
+
     exit()
 
 
-# there will be a list of apps that show on the website
-soup = BeautifulSoup(content, 'html.parser')
-
-# find the first class with the class name "search-dl"
-children = soup.findChildren('a', class_='col col-6 list')
-
-first_child = children[0]
-# get href of the first child
-app_href = first_child.get('href')
-print(app_href)
-
-# get the name of the child
-app_name = first_child.find('strong').text
-print(app_name)
-
-# hyphenate and format the app name into lowercase
-app_name_formatted = app_name.replace(" ", "-").lower()
-app_url = version_url + app_href
-
-# get string after the last '/'
-app_id = app_href.rsplit('/', 1)[1]
-
-
-app_response = requests.get(version_url + app_id,
-                            headers=headers, allow_redirects=True)
-
-if app_response.status_code != 200:
-    exit()
-
-app_soup = BeautifulSoup(app_response.content, 'html.parser')
-
-tbody_children = app_soup.findAll('a')
-
-links = []
-
-# tbody_children
-# loop through the children and get the href
-for item in tbody_children:
-    # get href of the child
-    link = item.get('href')
-    # if link contains "downwload"
-    if link.find("/download/") != -1:
-        # append the link to the list
-        links.append(base_url+link)
-
-# establish a connection to the first link
-download_response = requests.get(links[0],
-                                 headers=headers, allow_redirects=True)
-
-# locate the script, get the contents
-script_text = BeautifulSoup(
-    download_response.content, 'html.parser').findAll("script")
-
-# find the last script tag
-last_script = script_text[-2].contents[0]
-
-# query the token from the script
-token = re.findall("token','([^\']+)", last_script)[0]
-
-# make a request to the download link
-dl_response = requests.post(dl_url, data={'token': token}, headers=headers)
-if dl_response.status_code != 200:
-    exit()
-dl_response_json = dl_response.json()
-# get the download_link from the json response
-download_link = dl_response_json['download_link']
-
-# download the apk
-print("Downloading APK")
-exit()
-r = requests.get(download_link, allow_redirects=True)
-with open(app_name_formatted+'.apk', 'wb') as f:
-    f.write(r.content)
-print("APK Downloaded")
-
-
-exit()
+main()
